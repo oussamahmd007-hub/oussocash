26cfe62a548d08e5d4f0e500c71f5bd758aa5a5c
oussamahmd007-hub <oussamahmd007@gmail.com>
2026-06-09 20:48:04 +0000

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -4,7 +4,7 @@ const { sb, json, ONESIGNAL_APP_ID } = require('../lib/core');
 
 module.exports = async (req, res) => {
   try {
-    const rows = await sb('settings?select=key,value&key=in.(support_whatsapp,channel_url,coupon_today,coupon_tomorrow,coupon_yesterday)');
+    const rows = await sb('settings?select=key,value&key=in.(support_whatsapp,channel_url,coupon_today)');
     const map = {};
     (rows || []).forEach((r) => (map[r.key] = r.value));
     return json(res, 200, {
@@ -12,13 +12,9 @@ module.exports = async (req, res) => {
       channel_url: map.channel_url || '',
       onesignal_app_id: ONESIGNAL_APP_ID || '',
       sport_enabled: !!process.env.FOOTBALL_API_KEY,
-      coupons: {
-        today: map.coupon_today || '',
-        tomorrow: map.coupon_tomorrow || '',
-        yesterday: map.coupon_yesterday || '',
-      },
+      coupon: map.coupon_today || '',
     });
   } catch {
-    return json(res, 200, { support_whatsapp: '22232230404', channel_url: '', onesignal_app_id: ONESIGNAL_APP_ID || '', sport_enabled: !!process.env.FOOTBALL_API_KEY, coupons: { today:'', tomorrow:'', yesterday:'' } });
+    return json(res, 200, { support_whatsapp: '22232230404', channel_url: '', onesignal_app_id: ONESIGNAL_APP_ID || '', sport_enabled: !!process.env.FOOTBALL_API_KEY, coupon: '' });
   }
 };
